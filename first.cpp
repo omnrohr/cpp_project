4367a8283dbe4dc6b4935d73bb2f13b8eb17552e
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-09 19:41:46 +0200

    first commit

---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#pragma optimize("", off)
+int main()
+{
+
+    std::cout << "hellooo to the indless game, in this game every one is loser.";
+    std::cout << std::endl;
+    std::cout << "Just play for fun.... bye,,";
+    int a = 2;
+    int b = 6;
+    int c = 8;
+    int sum = a + b + c;
+    int multiply = a * b * c;
+    std::cout << std::endl;
+    std::cout << sum << std::endl
+              << multiply << std::endl;
+    return a - b;
+}
