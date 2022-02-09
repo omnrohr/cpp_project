b6d63febcae671e6fb4f6fc02fbc935e1164fc3a
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-09 21:12:43 +0200

    Start the triple x game

--- first.cpp
+++ first.cpp
@@ -10,22 +10,33 @@
 int main()
 {
     //  print welcome message to our game.
-    std::cout << "this is my first project in c++ so i am very pasionated" << std::endl;
+    std::cout << std::endl
+              << "this is my first project in c++ so i am very pasionated" << std::endl;
     std::cout << "So i Will understand it very will" << std::endl;
 
+    // get user input.
+    int GueseA, GueseB, GueseC;
+    std::cin >> GueseA >> GueseB >> GueseC;
+    std::cout << std::endl;
+
+    // Print
+    std::cout << "Your input is: " << GueseA << " " << GueseB << " " << GueseC << std::endl
+              << std::endl;
+
     // declare variables.
-    const int a = 1;
-    const int b = 2;
-    const int c = 3;
+    const int CodeA = 1;
+    const int CodeB = 2;
+    const int CodeC = 3;
 
     // calclulate sum and multiply result to terminal.
-    const int sum = a + b + c;
-    const int multi = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA + CodeB + CodeC;
 
     // print the result of sum and multiply to terminal.
-    std::cout << "we are caluculating 3 numbers " << a << " " << b << " " << c << std::endl;
-    std::cout << sum << std::endl
-              << multi << std::endl;
+    std::cout << std::endl
+              << "we are caluculating 3 numbers " << CodeA << " " << CodeB << " " << CodeC << std::endl;
+    std::cout << "sum: " << CodeSum << std::endl
+              << "multiply: " << CodeProduct << std::endl;
 
-    return a;
+    return 0;
 }
