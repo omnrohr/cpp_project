9d3fa390c0e1a93f5222cc4bdd3cd1c5eb8cddd7
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-09 20:46:54 +0200

    print more than line

--- first.cpp
+++ first.cpp
@@ -1,6 +1,31 @@
+#include <iostream>
 #pragma optimize("", off)
+
+/**
+ * @brief Her is my first code in c++. I like it becuse of cherno.
+ *
+ * @return int
+ */
+
 int main()
 {
-    int a = 1;
+    //  print welcome message to our game.
+    std::cout << "this is my first project in c++ so i am very pasionated" << std::endl;
+    std::cout << "So i Will understand it very will" << std::endl;
+
+    // declare variables.
+    const int a = 1;
+    const int b = 2;
+    const int c = 3;
+
+    // calclulate sum and multiply result to terminal.
+    const int sum = a + b + c;
+    const int multi = a * b * c;
+
+    // print the result of sum and multiply to terminal.
+    std::cout << "we are caluculating 3 numbers " << a << " " << b << " " << c << std::endl;
+    std::cout << sum << std::endl
+              << multi << std::endl;
+
     return a;
 }
