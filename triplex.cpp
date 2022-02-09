2d98be20f479636e9c453bc841c9d98c2e4ad1af
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-09 21:56:26 +0200

    stage 2

--- triplex.cpp
+++ triplex.cpp
@@ -5,9 +5,10 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue..." << std::endl;
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+
+    const int CodeA = 1;
+    const int CodeB = 2;
+    const int CodeC = 3;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -17,14 +18,26 @@ int main()
     std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    // declare guesses for user Input.
     int PlayerGuess;
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA;
     std::cin >> GuessB;
     std::cin >> GuessC;
 
+    std::cout << std::endl
+              << "You enterd " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    if (GuessSum == CodeSum && CodeProduct == GuessProduct)
+    {
+        std::cout << "You winn! " << std::endl;
+    }
+    else
+    {
+        std::cout << "Ops, you missed... later. " << std::endl;
+    }
     return 0;
 }
